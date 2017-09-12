5ae8f2fd20cff0fef265b6c229a4869ce3e63f1e
Jonny Kalambay <jonnyk20@gmail.com>
2017-09-12 18:38:25 +0000

    first commit

---
+++ express_server.js
@@ -0,0 +1,55 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.set("view engine", "ejs");
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) => {
+  res.end("hello :)")
+});
+
+app.get("/urls", (req, res) => {
+  // let templateVars = { urls: urlDatabase };
+  // res.render("urls_index", templateVars);
+  res.render("urls_index", {
+    urlDatabase: urlDatabase
+  })
+});
+
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/:id", (req, res) => {
+  res.render("urls_show", {
+    urlDatabase: urlDatabase,
+    selected: req.params.id
+  });
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
